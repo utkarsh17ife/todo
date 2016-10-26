1ffef1cdf40178d401fea30e2858b2556cb7959a
Utkarsh <utkarsh17ife@gmail.com>
2016-10-26 08:14:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+(function() {
+
+
+app = angular.module("todo",[]);
+
+app.controller('MainController',MainController);
+
+
+function MainController (){
+
+}
+
+
+
+})();
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+	res.send("hello from the other side");
+});
+
+app.listen(8080);
+console.log("server is up on 8080");
+
