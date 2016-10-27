7252d9c1d585d839aece61bdd3d0efd28baa5805
Utkarsh <utkarsh17ife@gmail.com>
2016-10-27 09:47:04 +0530

    second update

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 (function() {
 
-
 app = angular.module("todo",[]);
 
-app.controller('MainController',MainController);
+app.controller('MainController',MainController)
+.service('todoService',todoService);
+
+MainController.$inject = ['todoService'] 
+function MainController (todoService){
+	var ctrl = this;
+	
+	ctrl.items = todoService.viewItems();
+
+	ctrl.addItem = function(){
+		todoService.insertItems(ctrl.newItem);
+		ctrl.newItem = "";
+	}
+};
 
 
-function MainController (){
+function todoService(){
+	var todo = this;
+	var items = [];
 
-}
+	todo.insertItems = function(newItem){
+		items.push(newItem);
+	};
 
+	todo.viewItems = function(){
+		return items;
+	};
+};
 
 
 })();
