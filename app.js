5434adabe34a9ca8b1bb9cd4982bae3ebcdda6fb
Utkarsh <utkarsh17ife@gmail.com>
2016-10-30 14:56:52 +0530

    final

--- app.js
+++ app.js
@@ -5,50 +5,64 @@ app = angular.module("todo",[]);
 app.controller('MainController',MainController)
 .service('TodoService',TodoService)
 .constant('ApiBasePath', "https://todoserver.herokuapp.com");
+//.constant('ApiBasePath', "http://127.0.0.1:5000");
+
 
 MainController.$inject = ['TodoService'];
 function MainController (TodoService){
 	var ctrl = this;
-	
-	ctrl.items = TodoService.viewItems();
+	ctrl.items = [];
 
+	ctrl.init = function(){
+		var promise = TodoService.getItems();	
+		promise.then(function(response){
+			ctrl.items = response.data;
+			console.log(ctrl.items);
+		});
+	}
+	ctrl.init();
 	ctrl.addItem = function(){
-		TodoService.insertItems(ctrl.newItem);
-		ctrl.newItem = "";
+		var promise = TodoService.insertItems(ctrl.newItem);
+		promise.then(function(){
+			ctrl.init();
+			ctrl.newItem = "";
+		})
+	};
+	ctrl.taskDone = function(index){
+		var updateObj = {
+		uPropName : "status",
+		uPropValue : "done",
+		id : ctrl.items[index]._id
+		};
+		var promise = TodoService.updateItems(updateObj);
+		promise.then(function(response){
+			console.log(response)
+			ctrl.init();
+		});
 	}
-
-	ctrl.callToServer = function(){
-		console.log("call to server controller");
-		TodoService.getItems();
-	}
-
 };
-
 
 TodoService.$inject = ['$http','ApiBasePath'];
 function TodoService($http,ApiBasePath){
 	var service = this;
-	var items = [];
+	var item = [];
 
-	// todo.insertItems = function(newItem){
-	// 	$http.get(ApiBasePath ,function(data){
+	service.insertItems = function(newItem){	
+		var newItem = {"name":newItem};
+		var response = $http.post(ApiBasePath + "/" ,newItem);
+		return response;
+	};
 
-	// 	})
-	// 	items.push(newItem);
-	// };
 	service.getItems = function(){
-		var response = $http({
-          method: "GET",
-          url: (ApiBasePath + "/"),
-        }).then(function (response){
-        	console.log(response);	
-        });
-        
-	};
-	
-	service.viewItems = function(){
-		return items;
-	};
+		var response = $http.get(ApiBasePath);
+		return response;
+	};	
+
+	service.updateItems = function(updateObj){
+		var response = $http.put(ApiBasePath + "/", updateObj);
+		return response;
+	}
+
 };
 
 
--- server.js
+++ server.js
@@ -1,17 +1,68 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');  // pull information from HTML POST (express4)
+
+
+app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+mongoose.connect('mongodb://utkarsh:utkarsh@ds137267.mlab.com:37267/tododb');
+
+var Schema = mongoose.Schema;
+
+var TodoSchema = new Schema({
+	name: String,
+	status: String
+});
+
+var Todo = mongoose.model('Todo',TodoSchema);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 app.get('/', function(req, res){
-	res.send("Received the request");
+	Todo.find(function(err, todos){
+		if(err)
+		res.send(err);
+		res.json(todos);
+	});
 });
-
+app.post('/',function(req,res){
+	Todo.create({
+		name: req.body.name
+		}, function(err){
+			if(err)
+			res.send(err);
+		Todo.find(function(err, todos){
+			if(err)
+			res.send(err);
+			res.json(todos);
+		});
+	});	
+});
+app.put('/', function(req, res){
+	var updateObj = req.body; 
+	var uPropName = updateObj.uPropName; 
+	var update = {};
+	var condition = {};
+	update[uPropName] = updateObj.uPropValue;
+	condition['_id']  = updateObj.id;
+	Todo.update(condition, update, function(err, num){
+		if(err)
+			res.send(err);
+		res.status(200).send(num);
+	});
+	
+});
 app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, function(){
+	console.log("server is up");
+});
 
 
